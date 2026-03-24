0f9a6d7d4fdd9e5e88b39224051ebd85c0df0efc
BartoliniTheRealOne <bartosz.horna@gmail.com>
2026-03-24 12:40:03 +0100

    initial ball config added

--- Data/Ball.cs
+++ Data/Ball.cs
@@ -2,6 +2,41 @@
 {
     public class Ball
     {
+        private int number;
+        private int xPos;
+        private int yPos;
+        
+        public Ball(int number, int xPos, int yPos)
+        {
+            this.number = number;
+            this.xPos = xPos;
+            this.yPos = yPos;
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        public int GetXPos()
+        {
+            return xPos;
+        }
+
+        public int GetYPos()
+        {  
+            return yPos;
+        }
+
+        public void SetXPos(int xPos)
+        {
+            this.xPos = xPos;
+        }
+
+        public void SetYPos(int yPos)
+        {
+            this.yPos = yPos;
+        }
 
     }
 }
