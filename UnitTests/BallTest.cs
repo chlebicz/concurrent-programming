ff0c760b7b539a5b3cbbc307bdfb122a5dced2d4
BartoliniTheRealOne <bartosz.horna@gmail.com>
2026-03-24 12:45:38 +0100

    ball tests added

--- UnitTests/BallTest.cs
+++ UnitTests/BallTest.cs
@@ -5,9 +5,12 @@ namespace UnitTests
     public class BallTest
     {
         [Fact]
-        public void TwoPlusTwoIsFour()
+        public void CreateBallTest()
         {
-            Assert.Equal(2 + 2, 4);
+            Ball ball = new Ball(8, 5, 7);
+            Assert.Equal(8, ball.GetNumber());
+            Assert.Equal(5, ball.GetXPos());
+            Assert.Equal(7, ball.GetYPos());
         }
     }
 }
