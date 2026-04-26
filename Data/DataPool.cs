9f814223d0c07f9a977bb2bafa6c225af7a5ea26
BartoliniTheRealOne <bartosz.horna@gmail.com>
2026-04-26 21:05:36 +0200

    PrepareTest added

--- Data/DataPool.cs
+++ Data/DataPool.cs
@@ -8,24 +8,24 @@ namespace Data
     {
         public int XDim { get; }
         public int YDim { get; }
-        private List<IDataBall> balls;
-        public IReadOnlyCollection<IDataBall> Balls => balls.AsReadOnly();
+        private List<IDataBall> _balls;
+        public IReadOnlyCollection<IDataBall> Balls => _balls.AsReadOnly();
 
         public DataPool(int xDim, int yDim)
         {
             XDim = xDim;
             YDim = yDim;
-            balls = new List<IDataBall>();
+            _balls = new List<IDataBall>();
         }
 
         public void AddBall(IDataBall ball)
         {
-            this.balls.Add(ball);
+            _balls.Add(ball);
         }
 
         public void RemoveBall(IDataBall ball)
         {
-            this.balls.Remove(ball);
+            _balls.Remove(ball);
         }
     }
 }
--- LogicTest/LogicPoolTest.cs
+++ LogicTest/LogicPoolTest.cs
@@ -16,5 +16,21 @@ namespace LogicTest
             ILogicPool logicPool = new LogicPool(pool, factory);
             Assert.Equal(pool, logicPool.Pool);
         }
+
+        [Fact]
+
+        public void PrepareTest()
+        {
+            IBallFactory factory = new FakeBallFactory();
+            IDataPool pool = new FakeDataPool(100, 100);
+            ILogicPool logicPool = new LogicPool(pool, factory);
+            logicPool.Prepare(5);
+            Assert.Equal(5, logicPool.Balls.Count);
+            Assert.All(logicPool.Balls, ball =>
+            {
+                Assert.InRange(ball.Ball.X, ball.Ball.Radius, logicPool.Pool.XDim - ball.Ball.Radius);
+                Assert.InRange(ball.Ball.Y, ball.Ball.Radius, logicPool.Pool.YDim - ball.Ball.Radius);
+            });
+        }
     }
 }
