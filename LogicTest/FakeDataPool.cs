8e8e4513042542cb91f6c45db67e81bf340a1c28
BartoliniTheRealOne <bartosz.horna@gmail.com>
2026-04-26 20:47:51 +0200

    CreateLogicPullTest added

---
+++ LogicTest/FakeDataPool.cs
@@ -0,0 +1,33 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicTest
+{
+    internal class FakeDataPool : IDataPool
+    {
+        public int XDim { get; }
+        public int YDim { get; }
+
+        private List<IDataBall> balls;
+        public IReadOnlyCollection<IDataBall> Balls => balls.AsReadOnly();
+
+        public FakeDataPool(int xDim, int yDim)
+        {
+            XDim = xDim;
+            YDim = yDim;
+            balls = new List<IDataBall>();
+        }
+
+        public void AddBall(IDataBall ball)
+        {
+            this.balls.Add(ball);
+        }
+
+        public void RemoveBall(IDataBall ball)
+        {
+            this.balls.Remove(ball);
+        }
+    }
+}
---
+++ LogicTest/LogicPoolTest.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicTest
+{
+    public class LogicPoolTest
+    {
+        [Fact]
+        public void CreateLogicPoolTest()
+        {
+            IBallFactory factory = new FakeBallFactory();
+            IDataPool pool = new FakeDataPool(100, 100);
+            ILogicPool logicPool = new LogicPool(pool, factory);
+            Assert.Equal(pool, logicPool.Pool);
+        }
+    }
+}
