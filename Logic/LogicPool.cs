c581608132efdeb611eb30de75aba55d4f8758d9
BartoliniTheRealOne <bartosz.horna@gmail.com>
2026-05-18 18:46:42 +0200

    locks

--- Logic/LogicPool.cs
+++ Logic/LogicPool.cs
@@ -84,65 +84,78 @@ namespace Logic
                     continue;
                 }
 
-                //lock (otherBall)
-                //{
-                    if (!otherBall.CollidesWith(ball))
+                if (!otherBall.CollidesWith(ball))
+                {
+                    continue;
+                }
+
+                bool firstLockBall = ball.Ball.Number < otherBall.Ball.Number;
+                object firstLock = firstLockBall ? ball : otherBall;
+                object secondLock = firstLockBall ? otherBall : ball;
+
+                lock (firstLock)
+                {
+                    lock (secondLock)
                     {
-                        continue;
+
+                        if (!otherBall.CollidesWith(ball))
+                        {
+                            continue;
+                        }
+
+                        IDataBall b1 = ball.Ball;
+                        IDataBall b2 = otherBall.Ball;
+
+                        float dx = b1.X - b2.X;
+                        float dy = b1.Y - b2.Y;
+                        float distance = (float)Math.Sqrt(dx * dx + dy * dy);
+
+                        if (distance == 0) continue;
+
+                        float nx = dx / distance;
+                        float ny = dy / distance;
+
+                        float v1x = b1.DirectionX * b1.Velocity;
+                        float v1y = b1.DirectionY * b1.Velocity;
+                        float v2x = b2.DirectionX * b2.Velocity;
+                        float v2y = b2.DirectionY * b2.Velocity;
+
+                        float rvx = v1x - v2x;
+                        float rvy = v1y - v2y;
+
+                        float velAlongNormal = rvx * nx + rvy * ny;
+
+                        if (velAlongNormal > 0)
+                        {
+                            continue;
+                        }
+
+                        float j = -2 * velAlongNormal;
+                        j /= (1f / b1.Mass) + (1f / b2.Mass);
+
+                        float impulseX = j * nx;
+                        float impulseY = j * ny;
+
+                        float newV1x = v1x + (impulseX / b1.Mass);
+                        float newV1y = v1y + (impulseY / b1.Mass);
+                        float newV2x = v2x - (impulseX / b2.Mass);
+                        float newV2y = v2y - (impulseY / b2.Mass);
+
+                        b1.Velocity = (float)Math.Sqrt(newV1x * newV1x + newV1y * newV1y);
+                        if (b1.Velocity > 0)
+                        {
+                            b1.DirectionX = newV1x / b1.Velocity;
+                            b1.DirectionY = newV1y / b1.Velocity;
+                        }
+
+                        b2.Velocity = (float)Math.Sqrt(newV2x * newV2x + newV2y * newV2y);
+                        if (b2.Velocity > 0)
+                        {
+                            b2.DirectionX = newV2x / b2.Velocity;
+                            b2.DirectionY = newV2y / b2.Velocity;
+                        }
                     }
-
-                    IDataBall b1 = ball.Ball;
-                    IDataBall b2 = otherBall.Ball;
-
-                    float dx = b1.X - b2.X;
-                    float dy = b1.Y - b2.Y;
-                    float distance = (float)Math.Sqrt(dx * dx + dy * dy);
-
-                    if (distance == 0) continue;
-
-                    float nx = dx / distance;
-                    float ny = dy / distance;
-
-                    float v1x = b1.DirectionX * b1.Velocity;
-                    float v1y = b1.DirectionY * b1.Velocity;
-                    float v2x = b2.DirectionX * b2.Velocity;
-                    float v2y = b2.DirectionY * b2.Velocity;
-
-                    float rvx = v1x - v2x;
-                    float rvy = v1y - v2y;
-
-                    float velAlongNormal = rvx * nx + rvy * ny;
-
-                    if (velAlongNormal > 0)
-                    {
-                        continue;
-                    }
-
-                    float j = -2 * velAlongNormal;
-                    j /= (1f / b1.Mass) + (1f / b2.Mass);
-
-                    float impulseX = j * nx;
-                    float impulseY = j * ny;
-
-                    float newV1x = v1x + (impulseX / b1.Mass);
-                    float newV1y = v1y + (impulseY / b1.Mass);
-                    float newV2x = v2x - (impulseX / b2.Mass);
-                    float newV2y = v2y - (impulseY / b2.Mass);
-
-                    b1.Velocity = (float)Math.Sqrt(newV1x * newV1x + newV1y * newV1y);
-                    if (b1.Velocity > 0)
-                    {
-                        b1.DirectionX = newV1x / b1.Velocity;
-                        b1.DirectionY = newV1y / b1.Velocity;
-                    }
-
-                    b2.Velocity = (float)Math.Sqrt(newV2x * newV2x + newV2y * newV2y);
-                    if (b2.Velocity > 0)
-                    {
-                        b2.DirectionX = newV2x / b2.Velocity;
-                        b2.DirectionY = newV2y / b2.Velocity;
-                    }
-                //}
+                }
             }
         }
 
@@ -150,12 +163,12 @@ namespace Logic
         {
             while (!token.IsCancellationRequested)
             {
-                //lock (ball)
-                //{
+                lock (ball)
+                {
                     ball.Update();
                     _checkWallCollision(ball);
-                    _checkBallCollision(ball);
-                //}
+                }
+                _checkBallCollision(ball);
 
                 await Task.Delay(16, token);
             }
